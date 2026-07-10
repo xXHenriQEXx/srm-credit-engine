7b364937e84e3b1194fabd1b6e110f3cb5cb7956
Henrique <dev@example.com>
2026-07-10 04:56:36 +0000

    chore: scaffold spring boot project structure

---
+++ backend/src/main/java/com/srmasset/creditengine/SrmCreditEngineApplication.java
@@ -0,0 +1,11 @@
+package com.srmasset.creditengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrmCreditEngineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SrmCreditEngineApplication.class, args);
+    }
+}
